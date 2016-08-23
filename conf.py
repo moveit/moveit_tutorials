aac6d8ee910f92e6813695d23e3d147cc2957c11
Dave Coleman <davetcoleman@gmail.com>
2016-08-22 18:56:11 -0600

    Switch git account to ros-planning

--- conf.py
+++ conf.py
@@ -36,7 +36,7 @@ html_static_path = ['_static']
 
 html_context = {
     "display_github": True,
-    "github_user": "davetcoleman",
+    "github_user": "ros-planning",
     "github_repo": "moveit_tutorials",
     "github_version": "indigo-devel/doc/",
     "conf_py_path": "",
