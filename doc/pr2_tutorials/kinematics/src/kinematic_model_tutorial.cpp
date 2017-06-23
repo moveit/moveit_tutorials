a0629c096cafa4ee8e7aef266db12fcc28117bf2
Andrew Dahdouh <andrew.dahdouh@gmail.com>
2017-06-23 00:01:16 -0500

    Updated file to correct move_group for a UR10 Example

--- doc/pr2_tutorials/kinematics/src/kinematic_model_tutorial.cpp
+++ doc/pr2_tutorials/kinematics/src/kinematic_model_tutorial.cpp
@@ -43,7 +43,7 @@
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "right_arm_kinematics");
+  ros::init(argc, argv, "UR10_arm_kinematics");
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
@@ -70,16 +70,10 @@ int main(int argc, char **argv)
   robot_model::RobotModelPtr kinematic_model = robot_model_loader.getModel();
   ROS_INFO("Model frame: %s", kinematic_model->getModelFrame().c_str());
 
-  // Using the :moveit_core:`RobotModel`, we can construct a
-  // :moveit_core:`RobotState` that maintains the configuration
-  // of the robot. We will set all joints in the state to their
-  // default values. We can then get a
-  // :moveit_core:`JointModelGroup`, which represents the robot
-  // model for a particular group, e.g. the "right_arm" of the PR2
-  // robot.
+
   robot_state::RobotStatePtr kinematic_state(new robot_state::RobotState(kinematic_model));
   kinematic_state->setToDefaultValues();
-  const robot_state::JointModelGroup *joint_model_group = kinematic_model->getJointModelGroup("right_arm");
+  const robot_state::JointModelGroup *joint_model_group = kinematic_model->getJointModelGroup("manipulator");
 
   const std::vector<std::string> &joint_names = joint_model_group->getVariableNames();
 
