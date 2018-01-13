55fd83fca26adf511c6ec41654b1cfaf4cf02503
Andrew Dahdouh <andrew.dahdouh@gmail.com>
2018-01-13 08:25:35 -0800

    Updated file to remove conflict with ROS/Moveit remote.

--- doc/pr2_tutorials/kinematics/src/kinematic_model_tutorial.cpp
+++ doc/pr2_tutorials/kinematics/src/kinematic_model_tutorial.cpp
@@ -43,7 +43,7 @@
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "UR10_arm_kinematics");
+  ros::init(argc, argv, "right_arm_kinematics");
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
@@ -70,10 +70,16 @@ int main(int argc, char **argv)
   robot_model::RobotModelPtr kinematic_model = robot_model_loader.getModel();
   ROS_INFO("Model frame: %s", kinematic_model->getModelFrame().c_str());
 
-
+  // Using the :moveit_core:`RobotModel`, we can construct a
+  // :moveit_core:`RobotState` that maintains the configuration
+  // of the robot. We will set all joints in the state to their
+  // default values. We can then get a
+  // :moveit_core:`JointModelGroup`, which represents the robot
+  // model for a particular group, e.g. the "right_arm" of the PR2
+  // robot.
   robot_state::RobotStatePtr kinematic_state(new robot_state::RobotState(kinematic_model));
   kinematic_state->setToDefaultValues();
-  const robot_state::JointModelGroup *joint_model_group = kinematic_model->getJointModelGroup("manipulator");
+  const robot_state::JointModelGroup *joint_model_group = kinematic_model->getJointModelGroup("right_arm");
 
   const std::vector<std::string> &joint_names = joint_model_group->getVariableNames();
 
@@ -108,7 +114,7 @@ int main(int argc, char **argv)
   // "r_wrist_roll_link" which is the most distal link in the
   // "right_arm" of the robot.
   kinematic_state->setToRandomPositions(joint_model_group);
-  const Eigen::Affine3d &end_effector_state = kinematic_state->getGlobalLinkTransform("ee_link");
+  const Eigen::Affine3d &end_effector_state = kinematic_state->getGlobalLinkTransform("r_wrist_roll_link");
 
   /* Print end-effector pose. Remember that this is in the model frame */
   ROS_INFO_STREAM("Translation: " << end_effector_state.translation());
