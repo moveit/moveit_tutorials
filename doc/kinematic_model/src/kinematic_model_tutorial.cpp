cc8b5965e3afd731ef42675eaba78b6817468c03
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2018-05-05 15:49:35 -0500

    Clarified longest_valid_segment, also talked about max_waypoint_dist. (#165)
    * Clarified longest_valid_segment, also talked about max_waypoint_dist.
    
    * Fixed one of the travis warnings.

--- doc/kinematic_model/src/kinematic_model_tutorial.cpp
+++ doc/kinematic_model/src/kinematic_model_tutorial.cpp
@@ -123,13 +123,13 @@ int main(int argc, char** argv)
 
   // Inverse Kinematics
   // ^^^^^^^^^^^^^^^^^^
-  // We can now solve inverse kinematics (IK) for the arm of the
-  // Panda robot. To solve IK, we will need the following:
+  // We can now solve inverse kinematics (IK) for the Panda robot. 
+  // To solve IK, we will need the following:
   //
-  // * The desired pose of the end-effector (by default, this is the last link in the "panda_arm" chain):
-  //   end_effector_state that we computed in the step above.
-  // * The number of attempts to be made at solving IK: 10
-  // * The timeout for each attempt: 0.1 s
+  //  * The desired pose of the end-effector (by default, this is the last link in the "panda_arm" chain):
+  //    end_effector_state that we computed in the step above.
+  //  * The number of attempts to be made at solving IK: 10
+  //  * The timeout for each attempt: 0.1 s
   std::size_t attempts = 10;
   double timeout = 0.1;
   bool found_ik = kinematic_state->setFromIK(joint_model_group, end_effector_state, attempts, timeout);
