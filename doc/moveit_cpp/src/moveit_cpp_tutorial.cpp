c8db9c2db91fd32b52fa93e19aa09c92f9593db3
JafarAbdi <cafer.abdi@gmail.com>
2019-11-18 14:37:57 +0300

    Remove empty comments

--- doc/moveit_cpp/src/moveit_cpp_tutorial.cpp
+++ doc/moveit_cpp/src/moveit_cpp_tutorial.cpp
@@ -3,9 +3,9 @@
 // MoveitCpp
 #include <moveit/moveit_cpp/moveit_cpp.h>
 #include <moveit/moveit_cpp/planning_component.h>
-//
+
 #include <geometry_msgs/PointStamped.h>
-//
+
 #include <moveit_visual_tools/moveit_visual_tools.h>
 
 namespace rvt = rviz_visual_tools;
