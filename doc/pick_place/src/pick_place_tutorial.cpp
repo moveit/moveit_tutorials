1f6bb9093a01d0a9c2b62d49e0e6218acccc59c2
Mike Lautman <mike@picknik.com>
2020-02-03 13:47:45 -0800

    fixing wrong number

--- doc/pick_place/src/pick_place_tutorial.cpp
+++ doc/pick_place/src/pick_place_tutorial.cpp
@@ -89,10 +89,9 @@ void pick(moveit::planning_interface::MoveGroupInterface& move_group)
   // Setting grasp pose
   // ++++++++++++++++++++++
   // This is the pose of panda_link8. |br|
-  // From panda_link8 to the palm of the eef the distance is 0.058, the cube starts 0.01 before 5.0 (half of the length
-  // of the cube). |br|
-  // Therefore, the position for panda_link8 = 5 - (length of cube/2 - distance b/w panda_link8 and palm of eef - some
-  // extra padding)
+  // Make sure that when you set the grasp_pose, you are setting it to be the pose of the last link in
+  // your manipulator which in this case would be `"panda_link8"` You will have to compensate for the
+  // transform from `"panda_link8"` to the palm of the end effector.
   grasps[0].grasp_pose.header.frame_id = "panda_link0";
   tf2::Quaternion orientation;
   orientation.setRPY(-M_PI / 2, -M_PI / 4, -M_PI / 2);
@@ -142,9 +141,10 @@ void place(moveit::planning_interface::MoveGroupInterface& group)
 {
   // BEGIN_SUB_TUTORIAL place
   // TODO(@ridhwanluthra) - Calling place function may lead to "All supplied place locations failed. Retrying last
-  // location in
-  // verbose mode." This is a known issue and we are working on fixing it. |br|
-  // Create a vector of placings to be attempted, currently only creating single place location.
+  // location in verbose mode." This is a known issue. |br|
+  // |br|
+  // Ideally, you would create a vector of place locations to be attempted although in this example, we only create
+  // a single place location.
   std::vector<moveit_msgs::PlaceLocation> place_location;
   place_location.resize(1);
 
@@ -155,7 +155,7 @@ void place(moveit::planning_interface::MoveGroupInterface& group)
   orientation.setRPY(0, 0, M_PI / 2);
   place_location[0].place_pose.pose.orientation = tf2::toMsg(orientation);
 
-  /* While placing it is the exact location of the center of the object. */
+  /* For place location, we set the value to the exact location of the center of the object. */
   place_location[0].place_pose.pose.position.x = 0;
   place_location[0].place_pose.pose.position.y = 0.5;
   place_location[0].place_pose.pose.position.z = 0.5;
