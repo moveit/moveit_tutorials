0a1cba5795b68e7d904ef056349e6c51c12c4508
Mirko Kunze <mirko.kunze@web.de>
2021-01-19 10:17:30 +0100

    Update move_group_python_interface_tutorial.py
    test:
    ```
    import numpy as np
    from scipy.spatial.transform import Rotation as R
    from moveit_commander.conversions import list_to_pose
    
    for i in range(10000):
      rot0 = R.random()
      axis = np.random.rand(3)
      axis = axis / np.linalg.norm(axis)
      angle = 0.01
    
      rot1 = rot0 * R.from_rotvec(axis * angle)
    
      assert all_close(
        list_to_pose([0,0,0] + list(R.as_quat(rot0))),
        list_to_pose([0,0,0] + list(R.as_quat(rot1))),
        angle + 1e-8
      )
    
      assert not all_close(
        list_to_pose([0,0,0] + list(R.as_quat(rot0))),
        list_to_pose([0,0,0] + list(R.as_quat(rot1))),
        angle - 1e-8
      )
    
      # check with negative quaternion
      assert all_close(
        list_to_pose([0,0,0] + list(R.as_quat(rot0))),
        list_to_pose([0,0,0] + list(-R.as_quat(rot1))),
        angle + 1e-8
      )
    
      assert not all_close(
        list_to_pose([0,0,0] + list(R.as_quat(rot0))),
        list_to_pose([0,0,0] + list(-R.as_quat(rot1))),
        angle - 1e-8
      )
    ```

--- doc/move_group_python_interface/scripts/move_group_python_interface_tutorial.py
+++ doc/move_group_python_interface/scripts/move_group_python_interface_tutorial.py
@@ -51,7 +51,7 @@ import rospy
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
-from math import pi
+from math import pi, dist, fabs, cos
 from std_msgs.msg import String
 from moveit_commander.conversions import pose_to_list
 ## END_SUB_TUTORIAL
@@ -75,7 +75,13 @@ def all_close(goal, actual, tolerance):
     return all_close(goal.pose, actual.pose, tolerance)
 
   elif type(goal) is geometry_msgs.msg.Pose:
-    return all_close(pose_to_list(goal), pose_to_list(actual), tolerance)
+    x0, y0, z0, qx0, qy0, qz0, qw0 = pose_to_list(actual)
+    x1, y1, z1, qx1, qy1, qz1, qw1 = pose_to_list(goal)
+    # Euclidian distance
+    d = dist((x1, y1, z1), (x0, y0, z0))
+    # phi = angle between orientations
+    cos_phi_half = fabs(qx0*qx1 + qy0*qy1 + qz0*qz1 + qw0*qw1)
+    return d <= tolerance and cos_phi_half >= cos(tolerance / 2.0)
 
   return True
 
