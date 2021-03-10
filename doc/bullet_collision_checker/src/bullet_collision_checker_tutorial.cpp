5c8894560442f884e7535aae30cca5fafca8bf65
AndyZe <andyz@utexas.edu>
2021-03-10 14:04:11 -0600

    Clang format

--- doc/bullet_collision_checker/src/bullet_collision_checker_tutorial.cpp
+++ doc/bullet_collision_checker/src/bullet_collision_checker_tutorial.cpp
@@ -166,7 +166,8 @@ int main(int argc, char** argv)
     // Bullet. The second argument indicates that Bullet will be the exclusive collision detection algorithm; the
     // default FCL will not be available anymore. Having one exclusive collision detection algorithm helps performance
     // a bit and is much more common.
-    g_planning_scene->setActiveCollisionDetector(collision_detection::CollisionDetectorAllocatorBullet::create(), true /* exclusive */);
+    g_planning_scene->setActiveCollisionDetector(collision_detection::CollisionDetectorAllocatorBullet::create(),
+                                                 true /* exclusive */);
     // For understanding the interactive interactive_robot, please refer to the Visualizing Collisions tutorial.
     // CALL_SUB_TUTORIAL CCD
     // CALL_SUB_TUTORIAL CCD_2
@@ -211,7 +212,8 @@ int main(int argc, char** argv)
   // again set as the active collision detector.
   robot_model::RobotModelPtr robot_model = moveit::core::loadTestingRobotModel("panda");
   auto planning_scene = std::make_shared<planning_scene::PlanningScene>(robot_model);
-  planning_scene->setActiveCollisionDetector(collision_detection::CollisionDetectorAllocatorBullet::create(), /* exclusive = */ true);
+  planning_scene->setActiveCollisionDetector(collision_detection::CollisionDetectorAllocatorBullet::create(),
+                                             /* exclusive = */ true);
 
   // The box is added and the robot brought into its position.
   Eigen::Isometry3d box_pose{ Eigen::Isometry3d::Identity() };
