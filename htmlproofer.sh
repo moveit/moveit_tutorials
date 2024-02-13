3a4f7de4e8433fb87a65e306ca7de7f804eac2ee
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2024-02-13 18:34:13 +0100

    CI: Fix + Update actions

--- htmlproofer.sh
+++ htmlproofer.sh
@@ -7,9 +7,10 @@ export REPOSITORY_NAME=${PWD##*/}
 echo "Testing branch ${GITHUB_BASE_REF:-$GITHUB_HEAD_REF} of $REPOSITORY_NAME"
 
 # Install htmlpoofer
-sudo gem update --system --no-document
-gem --version
-sudo gem install html-proofer -v 3.19.4 # newer 4.x requires different cmdline options
+echo "gem $(gem --version)"
+gem install --user-install html-proofer -v 3.19.4 # newer 4.x requires different cmdline options
+PATH="$(ruby -r rubygems -e 'puts Gem.user_dir')/bin:$PATH"
+
 # Install ROS's version of sphinx
 sudo apt-get -qq install "ros-$ROS_DISTRO-rosdoc-lite"
 source "/opt/ros/$ROS_DISTRO/setup.bash"
